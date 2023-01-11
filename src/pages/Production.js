185ff1e2b24b5d59f838ebb11e65664885aa0c68
wnstjr <wnstjr8717@gmail.com>
2023-01-11 22:23:42 +0900

    ui sample

--- src/pages/Production.js
+++ src/pages/Production.js
@@ -1,7 +1,21 @@
+import LanguageWindow from "../components/LanguageWindow";
+import MediaWindow from "../components/MediaWindow";
+import TimelineWindow from "../components/TimelineWindow";
+
 const Production = () => {
-    return <div>
-        Production
-    </div>
+    return <>
+        <div style={{flexDirection: "row", display: 'flex'}}>
+            <div style={{display: 'flex', width: '100%', justifyContent: 'center'}}>
+                <div style={{flexDirection: 'column', display: 'flex', width: '30%'}}>
+                    <MediaWindow/>
+                </div>
+                <div style={{flexDirection: 'column', display: 'flex', width: '70%', alignItems: 'center'}}>
+                    <LanguageWindow/>
+                    <TimelineWindow/>
+                </div>
+            </div>
+        </div>
+    </>
 };
 
 export default Production

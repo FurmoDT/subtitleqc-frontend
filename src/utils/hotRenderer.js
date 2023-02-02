89849add7ae3c09fb13ef92ed3655e8490eba6d3
wnstjr <wnstjr8717@gmail.com>
2023-02-02 15:29:41 +0900

    text info

--- src/utils/hotRenderer.js
+++ src/utils/hotRenderer.js
@@ -6,9 +6,11 @@ export const tcOutValidator = (r, c, v, td) => {
 
 export const textValidator = (r, c, v, td) => {
     const label = document.createElement('label');
+    if (v) td.innerHTML = `<label>${v}</label>`
     label.style.float = 'right'
     label.style.fontSize = '10px'
     label.style.paddingLeft = '5px'
-    label.textContent = `len: ${v?.length || 0} cps: ${v?.length || 0}`;
+    label.style.textAlign = 'right'
+    label.textContent = `cps: ${v?.length || 0} ${v?.split('\n').map(val => `len: ${val.length}`).join('\n') || 'len: 0'}`;
     td.appendChild(label);
 }

333feec1dee0b96647d2c3f0578b6df256e8b218
wnstjr <wnstjr8717@gmail.com>
2023-02-02 09:32:27 +0900

    paddingleft

--- src/utils/hotRenderer.js
+++ src/utils/hotRenderer.js
@@ -8,6 +8,7 @@ export const textValidator = (r, c, v, td) => {
     const label = document.createElement('label');
     label.style.float = 'right'
     label.style.fontSize = '10px'
+    label.style.paddingLeft = '5px'
     label.textContent = `cps: ${v?.length || 0}`;
     td.appendChild(label);
 }

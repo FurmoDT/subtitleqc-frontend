7b6ed5ce42850b4675db4677c22fea12822f9179
wnstjr <wnstjr8717@gmail.com>
2023-02-14 23:46:37 +0900

    -

--- src/components/production/MenuToolbar.js
+++ src/components/production/MenuToolbar.js
@@ -1,4 +1,4 @@
-import {MDBBtn} from "mdb-react-ui-kit";
+import {MDBBtn, MDBIcon, MDBTooltip} from "mdb-react-ui-kit";
 import {toSrt} from "../../utils/fileParser";
 
 const MenuToolbar = (props) => {
@@ -10,17 +10,21 @@ const MenuToolbar = (props) => {
         borderWidth: 'thin',
         backgroundColor: 'lightgray'
     }}>
-        <MDBBtn style={{marginLeft: '5px'}} color={'secondary'} size={'sm'} onClick={() => {
-            props.languages.forEach((value) => {
-                const fileData = toSrt(props.cellDataRef.current, value)
-                const blob = new Blob([fileData], {type: "text/plain"})
-                const url = URL.createObjectURL(blob)
-                const link = document.createElement("a")
-                link.download = `${value.name}.srt`
-                link.href = url;
-                link.click();
-            })
-        }}>Download</MDBBtn>
+        <MDBTooltip tag='span' wrapperClass='d-inline-block' title='다운로드'>
+            <MDBBtn style={{marginLeft: '5px', color: 'black'}} size={'sm'} color={'secondary'} onClick={() => {
+                props.languages.forEach((value) => {
+                    const fileData = toSrt(props.cellDataRef.current, value)
+                    const blob = new Blob([fileData], {type: "text/plain"})
+                    const url = URL.createObjectURL(blob)
+                    const link = document.createElement("a")
+                    link.download = `${value.name}.srt`
+                    link.href = url;
+                    link.click();
+                })
+            }}>
+                <MDBIcon fas icon='download' size={'lg'}/>
+            </MDBBtn>
+        </MDBTooltip>
         <MDBBtn style={{marginLeft: '5px'}} outline size={'sm'} disabled>Shortcuts</MDBBtn>
         <label style={{position: 'absolute', left: '50%'}}>MenuToolbar</label>
     </div>

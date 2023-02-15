5cce23a398c082a6787404f263529232e0664ffe
wnstjr <wnstjr8717@gmail.com>
2023-02-15 17:20:43 +0900

    -

--- src/components/production/LanguageWindow.js
+++ src/components/production/LanguageWindow.js
@@ -83,6 +83,7 @@ const LanguageWindow = (props) => {
                 const [code, counter] = value.split('_').map((value, index) => (!index ? (languageCodes.hasOwnProperty(value) ? value : 'other') : value))
                 return {code: code, name: languageCodes[code] + (counter > 1 ? `(${counter})` : ''), counter: counter}
             }))
+            localStorage.setItem('cellData', JSON.stringify(props.cellDataRef.current))
         }
     }, [setLanguages, props.cellDataRef, props.languageFile, props.hotRef])
 

d02d3c4f3f44a3c6d064f82da721d91473e4430d
wnstjr <wnstjr8717@gmail.com>
2023-02-16 20:27:27 +0900

    -

--- src/components/production/MenuToolbar.js
+++ src/components/production/MenuToolbar.js
@@ -19,6 +19,11 @@ const MenuToolbar = (props) => {
         borderWidth: 'thin',
         backgroundColor: 'lightgray'
     }}>
+        <MDBTooltip tag='span' wrapperClass='d-inline-block' title='Project Setting'>
+            <MDBBtn style={{marginLeft: '5px', color: 'black'}} size={'sm'} color={'link'}>
+                <MDBIcon fas icon="info-circle" size={'2x'}/>
+            </MDBBtn>
+        </MDBTooltip>
         <MDBTooltip tag='span' wrapperClass='d-inline-block' title='New File'>
             <MDBBtn style={{marginLeft: '5px', color: 'black'}} size={'sm'} color={'link'} onClick={() => {
                 props.setLanguages([{code: 'xxXX', name: '기타 언어', counter: 1}])
@@ -26,18 +31,18 @@ const MenuToolbar = (props) => {
                 localStorage.removeItem('subtitle')
                 localStorage.removeItem('language')
             }}>
-                <MDBIcon far icon="file" size={'lg'}/>
+                <MDBIcon far icon="file" size={'2x'}/>
             </MDBBtn>
         </MDBTooltip>
         <MDBTooltip tag='span' wrapperClass='d-inline-block' title='Shortcut'>
             <MDBBtn style={{marginLeft: '5px', color: 'black'}} size={'sm'} color={'link'} disabled>
-                <MDBIcon fas icon="keyboard" size={'lg'}/>
+                <MDBIcon fas icon="keyboard" size={'2x'}/>
             </MDBBtn>
         </MDBTooltip>
         <MDBDropdown>
             <MDBTooltip tag='span' wrapperClass='d-inline-block' title='Download'>
                 <MDBDropdownToggle color={'link'}>
-                    <MDBIcon fas icon='download' size={'lg'} color={'dark'}/>
+                    <MDBIcon fas icon='download' size={'xl'} color={'dark'}/>
                 </MDBDropdownToggle>
             </MDBTooltip>
             <MDBDropdownMenu>

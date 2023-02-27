d7848da8a99eb4f6dc2c0957910013e92d3c3dde
wnstjr <wnstjr8717@gmail.com>
2023-02-27 23:41:16 +0900

    -

--- src/utils/hotRenderer.js
+++ src/utils/hotRenderer.js
@@ -24,12 +24,17 @@ export const tcOutValidator = (r, c, v, td, fontSize, instance) => {
 }
 
 export const textValidator = (r, c, v, td, fontSize) => {
+    td.style.position = 'relative'
+    td.style.paddingRight = '70px'
     const label = document.createElement('label');
     if (v) td.innerHTML = `<label style="text-overflow: ellipsis; display: block; white-space: pre; overflow: hidden; font-size: ${fontSize}">${v}</label>`
+    label.style.position = 'absolute'
+    label.style.top = 0
+    label.style.right = 0
     label.style.whiteSpace = 'pre'
-    label.style.float = 'right'
     label.style.fontSize = '10px'
     label.style.paddingLeft = '5px'
+    label.style.paddingRight = '5px'
     label.style.textAlign = 'right'
     label.style.color = 'lightgray'
     label.textContent = `cps: ${v?.length || 0} ${v?.split('\n').map(val => `len: ${val.length}`).join('\n') || 'len: 0'}`;

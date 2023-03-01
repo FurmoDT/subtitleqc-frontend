a10d3637cf4afc076499b12174440bfb2658bf1d
wnstjr <wnstjr8717@gmail.com>
2023-03-01 15:30:52 +0900

    render

--- src/components/production/MediaWindow.js
+++ src/components/production/MediaWindow.js
@@ -28,7 +28,7 @@ const MediaWindow = (props) => {
             if (curSubtitleIndex !== subtitleIndexRef.current) {
                 curSubtitleIndex = subtitleIndexRef.current
                 if (!props.fxToggle) setTdColor(subtitleIndexRef.current, true)
-            }
+            } else setTdColor(subtitleIndexRef.current, true)
             if (subtitleLabelRef.current.innerText !== nextSubtitle) subtitleLabelRef.current.innerText = nextSubtitle
         } else {
             if (curSubtitleIndex === subtitleIndexRef.current) {
@@ -44,7 +44,7 @@ const MediaWindow = (props) => {
             if (curFxIndex !== fxIndexRef.current) {
                 curFxIndex = fxIndexRef.current
                 if (props.fxToggle) setTdColor(fxIndexRef.current, true)
-            }
+            } else setTdColor(fxIndexRef.current, true)
             if (fxLabelRef.current.innerText !== nextSubtitle) fxLabelRef.current.innerText = nextSubtitle
         } else {
             if (curFxIndex === fxIndexRef.current) {
@@ -64,9 +64,15 @@ const MediaWindow = (props) => {
         subtitleIndexRef.current = bisect(props.cellDataRef.current.map((value) => tcToSec(value.start)), seconds)
         fxIndexRef.current = bisect(props.fxRef.current.map((value) => tcToSec(value.start)), seconds)
         if (!props.hotRef.current.getActiveEditor()?._opened) props.hotRef.current.scrollViewportTo(subtitleIndexRef.current - 1, 0)
-        afterRenderPromise().then()
         if (tcToSec(props.cellDataRef.current[subtitleIndexRef.current].start) !== seconds) subtitleIndexRef.current = Math.max(subtitleIndexRef.current - 1, 0)
         if (tcToSec(props.fxRef.current[fxIndexRef.current].start) !== seconds) fxIndexRef.current = Math.max(fxIndexRef.current - 1, 0)
+        let rendered = false
+        afterRenderPromise().then(() => {
+            setSubtitleLabel(seconds)
+            setFxLabel(seconds)
+            rendered = true
+        })
+        if (rendered) return
         setSubtitleLabel(seconds)
         setFxLabel(seconds)
     }, [props.cellDataRef, props.fxRef, props.hotRef, setSubtitleLabel, setFxLabel, afterRenderPromise])

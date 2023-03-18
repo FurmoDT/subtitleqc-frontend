7fa688a66c1fbdd449b82eb005fe3f4a6c412713
wnstjr <wnstjr8717@gmail.com>
2023-03-18 12:26:07 +0900

    -

--- src/utils/fileDownload.js
+++ src/utils/fileDownload.js
@@ -11,7 +11,7 @@ export const downloadFspx = (fileData) => {
     const blob = new Blob([JSON.stringify(fileData, null, 2)], {type: 'application/json;charset=utf-8'})
     const url = URL.createObjectURL(blob)
     const link = document.createElement("a")
-    link.download = `${fileData.projectDetail.name}.fspx`
+    link.download = `${fileData.projectDetail.name || '-'}.fspx`
     link.href = url;
     link.click();
 }

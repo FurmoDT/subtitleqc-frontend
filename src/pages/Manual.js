8088a877d7918846e9c9ae52cce2b9503a0695ce
wnstjr <wnstjr8717@gmail.com>
2023-03-28 13:52:34 +0900

    -

--- src/pages/Manual.js
+++ src/pages/Manual.js
@@ -1,5 +1,5 @@
 const Manual = () => {
-    window.open('https://docs.google.com/presentation/d/1uzpQOqp2TW38PZQ60BA7j3iPy0SZWOVjCk3d3IfOpG8/edit?usp=sharing');
+    window.open('https://docs.google.com/presentation/d/14zk9I95cR7y-E8ujeheKwtvlXKsnK4c0nUhUDyyUh0s/edit?usp=sharing');
     return <div>
         Manual
     </div>

4ec922bbfe854b581acaea66f7b1254a436fc055
wnstjr <wnstjr8717@gmail.com>
2023-04-11 08:43:56 +0900

    navbar toggler

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,28 @@
 import {
     MDBCollapse,
     MDBContainer,
+    MDBIcon,
     MDBNavbar,
     MDBNavbarBrand,
     MDBNavbarItem,
     MDBNavbarLink,
     MDBNavbarNav,
+    MDBNavbarToggler,
 } from 'mdb-react-ui-kit';
 import {Outlet, useNavigate} from "react-router-dom";
-import {useCallback} from "react";
+import {useCallback, useState} from "react";
 
 export default function Navbar(props) {
+    const [showNavNoTogglerSecond, setShowNavNoTogglerSecond] = useState(false);
     const navigate = useNavigate()
     return <div>
         <MDBNavbar expand='lg' light bgColor='light'>
             <MDBContainer fluid>
                 <MDBNavbarBrand href={`${props.basename}`}>SubtitleQC</MDBNavbarBrand>
-                <MDBCollapse navbar>
+                <MDBNavbarToggler onClick={() => setShowNavNoTogglerSecond(!showNavNoTogglerSecond)}>
+                    <MDBIcon icon='bars' fas/>
+                </MDBNavbarToggler>
+                <MDBCollapse navbar show={showNavNoTogglerSecond}>
                     <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
                         <MDBNavbarItem>
                             <MDBNavbarLink

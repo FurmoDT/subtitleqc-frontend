84def5c585ec7b4504047a856313834d3bab8ff7
wnstjr <wnstjr8717@gmail.com>
2023-05-11 01:53:12 +0900

    signup

--- src/pages/auth/SignupPage.js
+++ src/pages/auth/SignupPage.js
@@ -1,12 +1,18 @@
 import {MDBBtn, MDBInput} from "mdb-react-ui-kit";
-import {useRef} from "react";
+import {useContext, useRef} from "react";
+import axios from "../../utils/axios";
+import {AuthContext} from "../../utils/authContext";
+import {useNavigate} from "react-router-dom";
+import {HttpStatusCode} from "axios";
 
 const SignupPage = () => {
+    const navigate = useNavigate()
     const nameInputRef = useRef(null)
     const emailInputRef = useRef(null)
     const passwordInputRef = useRef(null)
     const confirmPasswordInputRef = useRef(null)
     const errorLabelRef = useRef(null)
+    const {setUserState} = useContext(AuthContext);
     return <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <div className={'auth-container'}>
             <div className={'auth-title'}>회원가입</div>
@@ -25,6 +31,26 @@ const SignupPage = () => {
                     errorLabelRef.current.innerText = '비밀번호가 일치하지 않습니다.'
                     return
                 }
+                axios.post(`/v1/auth/register`, {
+                    auth: {
+                        auth_type: 'email',
+                        user_email: emailInputRef.current.value,
+                        user_password: passwordInputRef.current.value
+                    }, user: {
+                        user_name: nameInputRef.current.value
+                    }
+                }).then((response) => {
+                    if (response.status === HttpStatusCode.Ok) {
+                        setUserState({accessToken: response.data.access_token})
+                        navigate('/')
+                    }
+                }).catch((reason) => {
+                    if (reason.response.status === HttpStatusCode.UnprocessableEntity) {
+                        errorLabelRef.current.innerText = '유효하지 않은 이메일입니다.'
+                    } else if (reason.response.status === HttpStatusCode.Conflict) {
+                        errorLabelRef.current.innerText = '이미 사용중인 이메일입니다.'
+                    }
+                })
             }}>회원가입</MDBBtn>
             <label ref={errorLabelRef} style={{fontSize: 13, color: 'red'}}/>
         </div>

836a450c996b588f07f9435f4abfe9eb3d1358a7
wnstjr <wnstjr8717@gmail.com>
2023-05-16 01:02:57 +0900

    default api call

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,5 +1,17 @@
+import {useContext, useEffect} from "react";
+import axios from "../utils/axios";
+import {AuthContext} from "../utils/authContext";
+
 const ProfilePage = () => {
-    return <div style={{height: '100vh', display: 'flex', justifyContent: 'center'}}>
+    const {userState} = useContext(AuthContext)
+    useEffect(() => {
+        if (userState.isAuthenticated) {
+            axios.get(`/v1/user/profile`).then((response) => {
+                console.log(response)
+            })
+        }
+    }, [userState])
+    return <div style={{height: 'calc(100vh - 60px)', display: 'flex', justifyContent: 'center'}}>
         Profile
     </div>
 };

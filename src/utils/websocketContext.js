07edc6e130a935361b9e11b654f34329585784d8
wnstjr <wnstjr8717@gmail.com>
2023-06-15 14:18:01 +0900

    websocket connect

--- src/utils/websocketContext.js
+++ src/utils/websocketContext.js
@@ -1,4 +1,4 @@
-import {createContext, useEffect, useRef, useState} from "react";
+import {createContext, useCallback, useEffect, useRef, useState} from "react";
 import {localWsUrl, wsUrl} from "./config";
 
 export const WebsocketContext = createContext(null);
@@ -7,15 +7,31 @@ export const WebsocketProvider = ({children}) => {
     const [isInitialized, setIsInitialized] = useState(false)
     const wsRef = useRef(null)
 
+    const connect = useCallback(() => {
+        return new Promise((resolve, reject) => {
+            if (wsRef.current) {
+                resolve()
+                return
+            }
+            wsRef.current = new WebSocket(`${process.env.NODE_ENV === 'development' ? localWsUrl : wsUrl}/v1/ws`)
+            wsRef.current.onopen = () => {
+                resolve()
+                console.log('ws opened')
+            }
+            wsRef.current.onclose = () => {
+                wsRef.current = null
+                console.log('ws closed')
+                setTimeout(connect, 5000)
+            }
+        })
+    }, [])
+
     useEffect(() => {
-        wsRef.current = new WebSocket(`${process.env.NODE_ENV === 'development' ? localWsUrl : wsUrl}/v1/ws`)
-        wsRef.current.onopen = () => console.log('ws opened')
-        wsRef.current.onclose = () => console.log('ws closed')
-        setIsInitialized(true)
+        connect().then(() => setIsInitialized(true))
         return () => {
             wsRef.current.close()
         }
-    }, [])
+    }, [connect])
 
     return isInitialized && <WebsocketContext.Provider value={{wsRef}}>
         {children}

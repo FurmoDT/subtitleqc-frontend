434c7ea62fdb54146edb85a0a4d5ca11dbe4ba92
wnstjr <wnstjr8717@gmail.com>
2023-06-20 16:13:03 +0900

    docx viewer

---
+++ src/pages/text/components/DocViewer.js
@@ -0,0 +1,26 @@
+import FileViewer from "react-file-viewer";
+import {useEffect, useState} from "react";
+import axios from "../../../utils/axios";
+
+const DocViewer = () => {
+    const [docxFile, setDocxFile] = useState(null)
+
+    useEffect(() => {
+        axios.get('https://s3.subtitleqc.ai/sample.docx', {
+            headers: {Authorization: null}, responseType: 'blob'
+        }).then((response) => {
+            const reader = new FileReader();
+            const file = new File([response.data], 'sample.xlsx', {type: response.headers['content-type']});
+            reader.onload = () => setDocxFile(reader.result)
+            reader.readAsDataURL(file);
+        })
+    }, [])
+
+    return <>
+        <div style={{height: '100%', overflow: 'auto'}}>
+            {docxFile && <FileViewer filePath={docxFile} fileType={'docx'}/>}
+        </div>
+    </>
+};
+
+export default DocViewer

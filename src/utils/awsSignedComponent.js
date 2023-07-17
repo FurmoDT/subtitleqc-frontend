063c70a9b57c205f2ba2402624dc414f3fd19188
wnstjr <wnstjr8717@gmail.com>
2023-07-17 16:59:21 +0900

    delete cookie after render

--- src/utils/awsSignedComponent.js
+++ src/utils/awsSignedComponent.js
@@ -3,14 +3,21 @@ import axios from "./axios";
 
 const AWSSignedComponent = ({component: Component, ...props}) => {
     const [isInitialized, setIsInitialized] = useState(false);
+    const [isRendered, setIsRendered] = useState(false);
+    const updateIsRendered = () => {setIsRendered(true)}
 
     useEffect(() => {
         axios.get('v1/aws/cloudfront/signed-cookies').then(() => {
             setIsInitialized(true)
         })
-    }, [props]);
+    }, [props.type]);
 
-    return isInitialized && <Component/>;
+    useEffect(() => {
+        if (isRendered) axios.delete('v1/aws/cloudfront/signed-cookies').then(()=>setIsRendered(false))
+    }, [isRendered])
+
+
+    return isInitialized && <Component updateIsRendered={updateIsRendered}/>;
 };
 
 export default AWSSignedComponent;

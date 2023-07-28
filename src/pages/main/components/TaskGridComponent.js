572ba7bb2dedd4483567c2996439bcea18b598e6
wnstjr <wnstjr8717@gmail.com>
2023-07-28 16:57:59 +0900

    task - work (MASTER - DETAIL)

--- src/pages/main/components/TaskGridComponent.js
+++ src/pages/main/components/TaskGridComponent.js
@@ -13,6 +13,41 @@ const TaskGridComponent = ({startAt, endAt}) => {
     const navigate = useNavigate()
     let columns
     const [rows, setRows] = useState(null)
+    const [taskAndWork, setTaskAndWork] = useState(null)
+
+    const groupBy = (arr, keyFunc) => {
+        return arr.reduce((result, current) => {
+            const key = keyFunc(current);
+            const pd = JSON.parse(current.pd)
+            if (!result[key]) {
+                result[key] = {
+                    task: {
+                        client: current.client_name,
+                        pm: current.pm_name,
+                        pd: Object.values(pd).join(','),
+                        projectCode: current.project_code,
+                        projectName: current.project_name,
+                        group: current.task_group_key,
+                        taskName: `${current.task_name}_${current.task_episode}`,
+                        taskType: fileType(current.task_file_extension),
+                        createdAt: formatTimestamp(current.task_created_at),
+                        dueDate: formatTimestamp(current.task_due_date),
+                        memo: current.task_memo,
+                        extra: {hashedId: current.task_hashed_id, pmId: current.pm_id, pd: pd},
+                    },
+                    work: []
+                };
+            }
+            if (current.work_id) result[key].work.push({
+                workType: workType[current.work_type],
+                worker: current.worker_name,
+                workDueDate: formatTimestamp(current.work_due_date),
+                sourceLanguage: languageCodes[current.work_source_language],
+                targetLanguage: languageCodes[current.work_target_language]
+            });
+            return result;
+        }, {});
+    }
 
     if (userState.user.userRole === 'client') {
         columns = [
@@ -30,7 +65,27 @@ const TaskGridComponent = ({startAt, endAt}) => {
             {key: '-', name: ''},
         ];
     } else if (/^(admin|pm)$/.test(userState.user.userRole)) {
+        const WorkGrid = ({hashedId}) => {
+            const work = taskAndWork[hashedId].work
+            return work.length ? (<DataGrid className={'rdg-light fill-grid'} style={{width: '50%', height: '100%'}}
+                                            rows={work} rowHeight={45} columns={[
+                {key: 'workType', name: '작업'}, {key: 'worker', name: '작업자'},
+                {key: 'sourceLanguage', name: '출발어'}, {key: 'targetLanguage', name: '도착어'},
+                {key: 'workDueDate', name: '마감일'}]}/>) : null
+        }
         columns = [
+            {
+                key: 'expanded', name: '', width: 40, minWidth: 40, maxWidth: 40, resizable: false,
+                colSpan: (args) => args.type === 'ROW' && args.row.type === 'DETAIL' ? 17 : undefined,
+                cellClass: (row) => row.type === 'DETAIL' ? 'rdg-detail-cell' : undefined,
+                renderCell: ({row, tabIndex, onRowChange}) => {
+                    if (row.type === 'DETAIL') {
+                        return <WorkGrid hashedId={row.hashedId}/>
+                    }
+                    return <div><span onClick={() => onRowChange({...row, expanded: !row.expanded})}>
+                        <span tabIndex={tabIndex}>{row.expanded ? '\u25BC' : '\u25B6'}</span></span></div>
+                }
+            },
             {key: 'no', name: 'No', width: 60},
             {key: 'client', name: 'Client'},
             {key: 'pm', name: 'PM'},
@@ -39,12 +94,6 @@ const TaskGridComponent = ({startAt, endAt}) => {
             {key: 'projectName', name: '프로젝트명'},
             {key: 'group', name: '그룹'},
             {key: 'taskName', name: '태스크명'},
-            {key: 'type', name: '소재', renderCell: (row) => <div>{row.row.type?.toUpperCase()}</div>},
-            {key: 'work', name: '작업'},
-            {key: 'worker', name: '작업자'},
-            {key: 'sourceLanguage', name: '출발어'},
-            {key: 'targetLanguage', name: '도착어'},
-            {key: 'workDueDate', name: '마감일'},
             {key: 'taskType', name: '소재', renderCell: (row) => <div>{row.row.taskType?.toUpperCase()}</div>},
             {key: 'requestedAt', name: '의뢰일'},
             {key: 'createdAt', name: '생성일'},
@@ -55,7 +104,7 @@ const TaskGridComponent = ({startAt, endAt}) => {
             {
                 key: '-',
                 name: '',
-                renderCell: (row) => Object.keys(row.row.extra.pd).includes(`${userState.user.userId}`) ?
+                renderCell: (row) => row.row.type === 'MASTER' && Object.keys(row.row.extra.pd).includes(`${userState.user.userId}`) ?
                     <><MDBBtn color={'link'} onClick={() => {
                         navigate(`/${row.row.taskType}/${row.row.extra.hashedId}`)
                     }} disabled={!row.row.taskType}>이동하기</MDBBtn>
@@ -101,30 +150,7 @@ const TaskGridComponent = ({startAt, endAt}) => {
         if (userState.user.userRole === 'client') {
         } else if (/^(admin|pm)$/.test(userState.user.userRole)) {
             axios.get('v1/project/task/pm', {params: {start_date: startAt, end_date: endAt}}).then((response) => {
-                console.log(response.data)
-                setRows(response.data.map((item, index) => {
-                    const pd = JSON.parse(item.pd)
-                    return {
-                        no: index + 1,
-                        client: item.client_name,
-                        pm: item.pm_name,
-                        pd: Object.values(pd).join(','),
-                        projectCode: item.project_code,
-                        projectName: item.project_name,
-                        group: item.task_group_key,
-                        taskName: `${item.task_name}_${item.task_episode}`,
-                        taskType: fileType(item.task_file_extension),
-                        workType: workType[item.work_type],
-                        worker: item.worker_name,
-                        workDueDate: formatTimestamp(item.work_due_date),
-                        sourceLanguage: languageCodes[item.work_source_language],
-                        targetLanguage: languageCodes[item.work_target_language],
-                        createdAt: formatTimestamp(item.task_created_at),
-                        dueDate: formatTimestamp(item.task_due_date),
-                        memo: item.task_memo,
-                        extra: {hashedId: item.task_hashed_id, pmId: item.pm_id, pd: pd}
-                    }
-                }))
+                setTaskAndWork(groupBy(response.data, (item) => item.task_hashed_id))
             })
         } else {
             axios.get('v1/project/task/worker', {params: {start_date: startAt, end_date: endAt}}).then((response) => {
@@ -151,12 +177,39 @@ const TaskGridComponent = ({startAt, endAt}) => {
     }, [userState.user.userRole, startAt, endAt])
 
     useEffect(() => {
+        if (!taskAndWork) return
+        setRows(Object.values(taskAndWork).reduce((result, current, currentIndex) => {
+            result.push({...current.task, expanded: true, type: 'MASTER', no: currentIndex + 1})
+            result.push({type: 'DETAIL', hashedId: current.task.extra.hashedId})
+            return result
+        }, []))
+    }, [taskAndWork])
+
+    useEffect(() => {
         if (rows) setInitialized(true)
     }, [rows])
 
+    function onRowsChange(rows, {indexes}) {
+        const row = rows[indexes[0]];
+        if (row.type === 'MASTER') {
+            if (row.expanded) {
+                rows.splice(indexes[0] + 1, 0, {
+                    type: 'DETAIL',
+                    hashedId: row.extra.hashedId
+                })
+            } else {
+                rows.splice(indexes[0] + 1, 1);
+            }
+            setRows(rows);
+        }
+    }
+
     return initialized &&
         <DataGrid className={'rdg-light fill-grid'} style={{height: '100%'}} columns={columns} rows={rows}
-                  rowHeight={(args) => 45}/>
+                  rowHeight={(args) => {
+                      return args.row.type === 'DETAIL' ? 98 + taskAndWork?.[args.row.hashedId].work.length * 45 : 45
+                  }}
+                  onRowsChange={onRowsChange}/>
 }
 
 export default TaskGridComponent

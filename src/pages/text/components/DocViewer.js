12e16e501699f7782fd509147f87f62dca302787
wnstjr <wnstjr8717@gmail.com>
2023-09-04 11:22:51 +0900

    -

--- src/pages/text/components/DocViewer.js
+++ src/pages/text/components/DocViewer.js
@@ -2,14 +2,14 @@ import {useEffect, useRef} from "react";
 import WebViewer from '@pdftron/webviewer';
 
 const DocViewer = ({textFile}) => {
-    const viewer = useRef(null);
+    const viewerRef = useRef(null);
 
     useEffect(() => {
         WebViewer({
             path: '/webviewer',
             licenseKey: 'demo:1691321479102:7c5cde3a030000000098e5ae4b3bfe9f2e04b89f2529305bd83b50c6d7',
             disabledElements: ['menuButton', 'leftPanelButton', 'panToolButton', 'toggleNotesButton', 'selectToolButton', 'toolsHeader', 'ribbons'],
-        }, viewer.current,).then((instance) => {
+        }, viewerRef.current).then((instance) => {
             const {documentViewer} = instance.Core;
             instance.UI.loadDocument(textFile, {withCredentials: true})
             instance.UI.setZoomLevel(1)
@@ -19,7 +19,7 @@ const DocViewer = ({textFile}) => {
 
     return <>
         <div className="MyComponent" style={{width: '100%', height: '100%'}}>
-            <div className="webviewer" ref={viewer} style={{height: "100%"}}/>
+            <div className="webviewer" ref={viewerRef} style={{height: "100%"}}/>
         </div>
     </>
 };

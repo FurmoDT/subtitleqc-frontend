021376df2a8d26a69185335f156e7f0c4de7da19
wnstjr <wnstjr8717@gmail.com>
2023-11-09 00:54:47 +0900

    custom control (volume control undone)

--- src/pages/production/components/MediaWindow.js
+++ src/pages/production/components/MediaWindow.js
@@ -1,7 +1,12 @@
 import ReactPlayer from "react-player";
 import {useCallback, useEffect, useRef, useState} from "react";
-import {bisect, tcToSec} from "../../../utils/functions";
-import {MDBBtn, MDBDropdown, MDBDropdownItem, MDBDropdownMenu, MDBDropdownToggle, MDBIcon} from "mdb-react-ui-kit";
+import {bisect, secToTc, tcToSec} from "../../../utils/functions";
+import {MDBDropdown, MDBDropdownItem, MDBDropdownMenu, MDBDropdownToggle, MDBIcon, MDBRange} from "mdb-react-ui-kit";
+import RangeSlider from 'react-range-slider-input';
+import '../../../css/RangeSlider.css';
+import {BsPauseFill, BsPlayFill} from "react-icons/bs";
+import {SlSpeedometer} from "react-icons/sl";
+import {HiMiniSpeakerWave, HiMiniSpeakerXMark} from "react-icons/hi2";
 
 let subtitleLanguage = null
 
@@ -9,6 +14,12 @@ const MediaWindow = ({setVideo, ...props}) => {
     const subtitleLabelRef = useRef(null)
     const curSubtitleIndexRef = useRef(-1)
     const [t] = useState(Date.now())
+    const [seek, setSeek] = useState(0)
+    const [duration, setDuration] = useState(0)
+    const [isMuted, setIsMuted] = useState(false)
+    const [isPlaying, setIsPlaying] = useState(false)
+    const volumeRef = useRef(null)
+
     const setLabel = useCallback((seconds, start, end, forceSelect, isSeek) => {
         let curIndex = curSubtitleIndexRef
         let targetIndex = props.subtitleIndexRef.current
@@ -38,6 +49,7 @@ const MediaWindow = ({setVideo, ...props}) => {
     }, [props.cellDataRef, props.subtitleIndexRef, props.hotRef])
 
     const onSeek = useCallback((seconds) => {
+        setSeek(seconds)
         props.subtitleIndexRef.current = bisect(props.cellDataRef.current.map((value) => tcToSec(value.start)), seconds)
         if (tcToSec(props.cellDataRef.current[props.subtitleIndexRef.current].start) !== seconds) props.subtitleIndexRef.current = Math.max(props.subtitleIndexRef.current - 1, 0)
         const {start: subtitleStart, end: subtitleEnd} = props.cellDataRef.current[props.subtitleIndexRef.current]
@@ -46,14 +58,18 @@ const MediaWindow = ({setVideo, ...props}) => {
         props.isFromLanguageWindowRef.current = false
     }, [props.cellDataRef, props.isFromLanguageWindowRef, setLabel, props.subtitleIndexRef, props.waveformRef])
 
+    const onDuration = useCallback((duration) => setDuration(duration), [])
+
     const onProgress = useCallback((state) => {
+        setSeek(state.playedSeconds)
         const {start: subtitleStart, end: subtitleEnd} = props.cellDataRef.current[props.subtitleIndexRef.current]
         setLabel(state.playedSeconds, tcToSec(subtitleStart), tcToSec(subtitleEnd), true, false)
         if (document.getElementById('playheadCenter-checkbox').checked) props.waveformRef.current?.views.getView('zoomview').updateWaveform(props.waveformRef.current?.views.getView('zoomview')._playheadLayer._playheadPixel - props.waveformRef.current?.views.getView('zoomview').getWidth() / 2)
         if (state.playedSeconds >= tcToSec(props.cellDataRef.current[props.subtitleIndexRef.current].end)) props.subtitleIndexRef.current += 1
     }, [props.cellDataRef, setLabel, props.subtitleIndexRef, props.waveformRef])
 
-    const onPlayPause = useCallback(() => {
+    const onPlayPause = useCallback((event) => {
+        setIsPlaying(event?.type !== 'pause')
         const curIndex = props.subtitleIndexRef.current
         const [start, end] = props.hotRef.current.getDataAtRow(curIndex).slice(0, 2)
         const currentTime = props.playerRef.current.getCurrentTime().toFixed(3)
@@ -78,45 +94,93 @@ const MediaWindow = ({setVideo, ...props}) => {
         subtitleLabelRef.current.innerHTML = ''
     }, [props.mediaFile])
 
-    return <div
-        className={'w-100 h-100 d-flex justify-content-center align-items-end position-relative overflow-hidden'}
-        style={{borderStyle: 'solid', borderWidth: 'thin', backgroundColor: 'black'}} onClick={(event) => {
-        const video = props.playerRef.current?.getInternalPlayer()
-        if (video && event.target.tagName === 'VIDEO') video.paused ? video.readyState && video.play() : video.pause()
-    }}>
-        <ReactPlayer ref={props.playerRef} width={'100%'} height={'100%'} progressInterval={1} onProgress={onProgress}
-                     onReady={onReady} onPlay={onPlayPause} onPause={onPlayPause} onSeek={onSeek} controls={true}
-                     url={props.mediaFile?.startsWith('https://s3.subtitleqc.ai') ? `${props.mediaFile}?t=${t}` : props.mediaFile}
-                     config={{
-                         file: {
-                             attributes: {
-                                 controlsList: 'nodownload nofullscreen',
-                                 disablePictureInPicture: true,
-                                 onContextMenu: e => e.preventDefault()
-                             }
-                         }
-                     }}/>
-        <span className={'position-absolute pe-none top-0 start-0 ms-1'} style={{color: 'white', fontSize: '0.8rem'}}>
-            {props.mediaInfo?.framerate && `${props.mediaInfo.framerate} fps`}</span>
-        <span ref={subtitleLabelRef} className={'span-subtitle'}/>
-        <div className={'position-absolute top-0 end-0'}>
-            <MDBDropdown className={'d-flex justify-content-end'}>
-                <MDBDropdownToggle color={'link'}>
-                    <MDBBtn tag='a' color={'none'}>
-                        <MDBIcon fas icon='globe' color={'white'} size={'lg'}/>
-                    </MDBBtn>
-                </MDBDropdownToggle>
-                <MDBDropdownMenu>
-                    {props.languages.filter((value) => value.code.match(/^[a-z]{2}[A-Z]{2}$/)).map((value) => {
-                        return <MDBDropdownItem link key={`${value.code}_${value.counter}`} onClick={() => {
-                            subtitleLanguage = `${value.code}_${value.counter}`
-                            if (props.playerRef.current.getInternalPlayer()?.paused) props.playerRef.current.seekTo(props.playerRef.current.getCurrentTime(), 'seconds')
-                        }}>{value.name}</MDBDropdownItem>
-                    })}
-                </MDBDropdownMenu>
-            </MDBDropdown>
+    return <>
+        <div className={'w-100 position-relative'} style={{height: 'calc(100% - 3rem)', backgroundColor: 'black'}}
+             onClick={() => {
+                 const video = props.playerRef.current?.getInternalPlayer()
+                 if (video) isPlaying ? video.pause() : video.play()
+             }}>
+            <ReactPlayer ref={props.playerRef} width={'100%'} height={'100%'} progressInterval={1} muted={isMuted}
+                         onProgress={onProgress} onReady={onReady} onDuration={onDuration}
+                         onPlay={onPlayPause} onPause={onPlayPause} onSeek={onSeek}
+                         url={props.mediaFile?.startsWith('https://s3.subtitleqc.ai') ? `${props.mediaFile}?t=${t}` : props.mediaFile}
+                         config={{file: {attributes: {onContextMenu: e => e.preventDefault()}}}}/>
+            <div className={'w-100 h-100 position-absolute top-0'}/>
+            <span className={'span-framerate mx-1'}>
+                {props.mediaInfo?.framerate && `${props.mediaInfo.framerate} fps`}</span>
+            <span ref={subtitleLabelRef} className={'span-subtitle'}/>
         </div>
-    </div>
+        <div className={'w-100 h-100'} style={{backgroundColor: 'black'}}>
+            <div className={'d-flex align-items-center px-3'} style={{height: '1rem'}}>
+                <RangeSlider className="single-thumb" thumbsDisabled={[true, false]} rangeSlideDisabled={true}
+                             value={[0, seek]} max={duration}
+                             onInput={value => props.playerRef.current.seekTo(value[1], 'seconds')}/>
+
+            </div>
+            <div className={'d-flex justify-content-between px-3'} style={{height: '2rem'}}>
+                <div className={'h-100 d-flex flex-nowrap align-items-center'}>
+                    {isPlaying ?
+                        <BsPauseFill className={'button-icon'} size={20}
+                                     onClick={event => props.playerRef.current.getInternalPlayer().pause()}/> :
+                        <BsPlayFill className={'button-icon'} size={20}
+                                    onClick={event => props.playerRef.current.getInternalPlayer()?.play()}/>}
+                    <span className={'span-duration ms-2'}>{`${secToTc(seek)} / ${secToTc(duration)}`}</span>
+                </div>
+                <div className={'h-100 d-flex flex-nowrap align-items-center'}>
+                    <div className={'position-relative d-flex justify-content-center me-4'}
+                         onMouseEnter={() => volumeRef.current.classList.remove('d-none')}
+                         onMouseLeave={() => volumeRef.current.classList.add('d-none')}>
+                        {isMuted ?
+                            <HiMiniSpeakerXMark className={'button-icon'} size={20}
+                                                onClick={() => {
+                                                    const internalPlayer = props.playerRef.current.getInternalPlayer()
+                                                    setIsMuted(false)
+                                                    if (internalPlayer) internalPlayer.muted = false
+                                                }}/> :
+                            <HiMiniSpeakerWave className={'button-icon'} size={20}
+                                               onClick={() => {
+                                                   const internalPlayer = props.playerRef.current.getInternalPlayer()
+                                                   setIsMuted(true)
+                                                   if (internalPlayer) internalPlayer.muted = true
+                                               }}/>}
+                        <div ref={volumeRef} className={'position-absolute'} style={{bottom: 20}}>
+                            <div style={{height: '140px', padding: '20px'}}>
+
+                            </div>
+                        </div>
+                    </div>
+                    <MDBDropdown className={'me-4'} dropup>
+                        <MDBDropdownToggle tag={'section'} className={'button-icon d-flex align-items-center'}>
+                            <SlSpeedometer size={20}/>
+                        </MDBDropdownToggle>
+                        <MDBDropdownMenu style={{minWidth: '5rem', height: '10rem', overflowY: 'scroll'}}
+                                         responsive={'end'}>
+                            <div className={'text-center fw-bold'}>재생 속도</div>
+                            {Array.from({length: (2 - 0.25) / 0.25 + 1}, (_, i) => 0.25 + 0.25 * i).map(value =>
+                                <MDBDropdownItem link key={value}
+                                                 onClick={() => {
+                                                     const internalPlayer = props.playerRef.current.getInternalPlayer()
+                                                     if (internalPlayer) internalPlayer.playbackRate = value
+                                                 }}>{`${value}`}</MDBDropdownItem>)}
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
+                    <MDBDropdown dropup>
+                        <MDBDropdownToggle tag={'section'} className={'button-icon d-flex align-items-center'}>
+                            <MDBIcon fas icon='globe'/>
+                        </MDBDropdownToggle>
+                        <MDBDropdownMenu style={{minWidth: '7rem'}} responsive={'end'}>
+                            <div className={'text-center fw-bold'}>언어 선택</div>
+                            {props.languages.filter((value) => value.code.match(/^[a-z]{2}[A-Z]{2}$/)).map(value =>
+                                <MDBDropdownItem link key={`${value.code}_${value.counter}`} onClick={() => {
+                                    subtitleLanguage = `${value.code}_${value.counter}`
+                                    if (props.playerRef.current.getInternalPlayer()?.paused) props.playerRef.current.seekTo(props.playerRef.current.getCurrentTime(), 'seconds')
+                                }}>{value.name}</MDBDropdownItem>)}
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
+                </div>
+            </div>
+        </div>
+    </>
 }
 
 export default MediaWindow

735f50d2c05f6603bad22fd7fb7e1f5816e16349
wnstjr <wnstjr8717@gmail.com>
2023-11-14 11:39:21 +0900

    -

--- src/pages/text/components/QuillEditor.js
+++ src/pages/text/components/QuillEditor.js
@@ -106,9 +106,7 @@ const QuillEditor = ({editorType, taskHashedId, targetLanguage, iceservers, conn
 
         persistence.once('synced', () => {
             if (taskHashedId) {
-                axios.get('v1/task/content', {
-                    params: {hashed_id: taskHashedId, room_type: editorType, target_language: targetLanguage.value}
-                }).then((r) => {
+                axios.get('v1/task/content', {params: {hashed_id: taskHashedId}}).then((r) => {
                     if (r.data) Y.applyUpdate(yDoc, toUint8Array(r.data.task_crdt))
                 }).finally(() => initializedRef.current = true)
             }

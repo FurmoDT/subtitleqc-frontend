730a6bb2482b9a4e0062a0d3318646430ad1f530
wnstjr <wnstjr8717@gmail.com>
2023-12-27 15:00:38 +0900

    -

--- src/pages/production/components/TimelineWindow.js
+++ src/pages/production/components/TimelineWindow.js
@@ -3,13 +3,9 @@ import Peaks from 'peaks.js';
 import {bisect, secToTc, tcToSec} from "../../../utils/functions";
 import {MDBBtn, MDBCheckbox, MDBIcon, MDBSpinner} from "mdb-react-ui-kit";
 
-const checkboxLabelStyle = {
-    fontSize: 12, userSelect: 'none', display: 'flex', alignItems: 'center', color: 'white', marginLeft: -5
-}
-
 const TimelineWindow = ({resetSegments, ...props}) => {
-    const waveformRef = useRef(null);
-    const overviewRef = useRef(null);
+    const zoomviewContainerRef = useRef(null);
+    const overviewContainerRef = useRef(null);
     const statusRef = useRef(null);
     const spinnerRef = useRef(null);
     const warningRef = useRef(null);
@@ -17,19 +13,19 @@ const TimelineWindow = ({resetSegments, ...props}) => {
     const moveCursorRef = useRef(null)
 
     const onWheel = useCallback((e) => {
+        const waveform = props.waveformRef.current
         if (e.ctrlKey) {
             e.preventDefault()
-            if (e.deltaY > 0) props.waveformRef.current?.zoom.zoomOut()
-            else props.waveformRef.current?.zoom.zoomIn()
+            if (e.deltaY > 0) waveform.zoom.zoomOut()
+            else waveform.zoom.zoomIn()
         } else if (e.shiftKey) {
             e.preventDefault()
             if (e.deltaY > 0) amplitudeScale.current = Math.max(amplitudeScale.current - 0.5, 0.5)
             else amplitudeScale.current = Math.min(amplitudeScale.current + 0.5, 5)
-            props.waveformRef.current.views.getView('zoomview')?.setAmplitudeScale(amplitudeScale.current)
+            waveform.views.getView('zoomview').setAmplitudeScale(amplitudeScale.current)
         } else {
-            const player = props.waveformRef.current?.player
-            if (e.deltaY > 0) player?.seek(player.getCurrentTime() - 1)
-            else player?.seek(player.getCurrentTime() + 1)
+            if (e.deltaY > 0) waveform.views.getView('zoomview').scrollWaveform({seconds: -1})
+            else waveform.views.getView('zoomview').scrollWaveform({seconds: 1})
         }
     }, [props.waveformRef])
     const afterSeekedPromise = useCallback(() => {
@@ -66,7 +62,7 @@ const TimelineWindow = ({resetSegments, ...props}) => {
             },
             withCredentials: true,
             zoomview: {
-                container: waveformRef.current,
+                container: zoomviewContainerRef.current,
                 waveformColor: 'yellow',
                 showPlayheadTime: true,
                 playheadColor: 'white',
@@ -75,7 +71,7 @@ const TimelineWindow = ({resetSegments, ...props}) => {
                 playheadClickTolerance: -1 // uncaught
             },
             overview: {
-                container: overviewRef.current,
+                container: overviewContainerRef.current,
                 waveformColor: 'yellow',
                 playheadColor: 'white',
                 highlightColor: 'black',
@@ -96,19 +92,6 @@ const TimelineWindow = ({resetSegments, ...props}) => {
             if (peaks) {
                 props.waveformRef.current = peaks
                 peaks.segments.add(resetSegments())
-                peaks.on('zoomview.click', (event) => {
-                    afterSeekedPromise().then(() => {
-                        props.waveformRef.current?.player.pause()
-                        if (event.evt.ctrlKey) {
-                            const time = peaks.player.getCurrentTime()
-                            if (!peaks.segments.find(time, time + 1).length) {
-                                const addIndex = bisect(props.hotRef.current.getSourceDataAtCol('start').map(v => tcToSec(v)).filter(value => !isNaN(value)), time)
-                                props.hotRef.current.alter('insert_row', addIndex, 1)
-                                props.hotRef.current.setDataAtCell([[addIndex, 0, secToTc(time)], [addIndex, 1, secToTc(time + 1)]])
-                            }
-                        }
-                    })
-                })
                 peaks.on("segments.mouseenter", (event) => {
                     if (event.segment.editable) {
                         moveCursorRef.current = event.evt.target
@@ -142,19 +125,6 @@ const TimelineWindow = ({resetSegments, ...props}) => {
                     if (props.hotRef.current.getDataAtCell(row, 1) !== end) cells.push([row, 1, end])
                     props.hotRef.current.setDataAtCell(cells, 'timelineWindow')
                 })
-                peaks.on("segments.click", (event) => {
-                    peaks.views.getView('zoomview')?.enableSegmentDragging(false)
-                    moveCursorRef.current = event.evt.target
-                    if (props.selectedSegment.current === event.segment) {
-                        moveCursorRef.current.style.cursor = 'default'
-                        props.selectedSegment.current.update({color: 'white', editable: false})
-                        props.selectedSegment.current = null
-                        return
-                    }
-                    props.hotRef.current.selectRows(props.hotRef.current.getSourceDataAtCol('rowId').indexOf(event.segment.id))
-                    peaks.views.getView('zoomview')?.enableSegmentDragging(true)
-                    moveCursorRef.current.style.cursor = 'move'
-                })
                 peaks.on("segments.remove", (event) => {
                     event.segments.forEach(value => {
                         if (value === props.selectedSegment.current) {
@@ -162,6 +132,8 @@ const TimelineWindow = ({resetSegments, ...props}) => {
                         }
                     })
                 })
+                let isDoubleClick = false;
+                let clickTimer;
                 peaks.on('peaks.ready', () => {
                     if (!props.playerRef.current.getInternalPlayer()?.src.startsWith(props.video)) {
                         props.waveformRef.current?.destroy()
@@ -169,25 +141,63 @@ const TimelineWindow = ({resetSegments, ...props}) => {
                     } else {
                         setStatusDisplay('loaded')
                     }
+                    peaks.on('zoomview.click', (event) => {
+                        if (!isDoubleClick) {
+                            isDoubleClick = true
+                            clickTimer = setTimeout(() => {
+                                isDoubleClick = false;
+                                peaks.player.seek(event.time)
+                                afterSeekedPromise().then(() => {
+                                    props.waveformRef.current?.player.pause()
+                                    if (event.evt.ctrlKey) {
+                                        const time = peaks.player.getCurrentTime()
+                                        if (!peaks.segments.find(time, time + 1).length) {
+                                            const addIndex = bisect(props.hotRef.current.getSourceDataAtCol('start').map(v => tcToSec(v)).filter(value => !isNaN(value)), time)
+                                            props.hotRef.current.alter('insert_row', addIndex, 1)
+                                            props.hotRef.current.setDataAtCell([[addIndex, 0, secToTc(time)], [addIndex, 1, secToTc(time + 1)]])
+                                        }
+                                    }
+                                })
+                            }, 300);
+                        } else {
+                            clearTimeout(clickTimer)
+                            isDoubleClick = false
+                            const segment = peaks.segments.find(event.time, event.time)[0]
+                            if (segment) {
+                                peaks.views.getView('zoomview').enableSegmentDragging(false)
+                                moveCursorRef.current = event.evt.target
+                                if (props.selectedSegment.current === segment) {
+                                    moveCursorRef.current.style.cursor = 'default'
+                                    props.selectedSegment.current.update({color: 'white', editable: false})
+                                    props.selectedSegment.current = null
+                                    return
+                                }
+                                props.hotRef.current.selectRows(props.hotRef.current.getSourceDataAtCol('rowId').indexOf(segment.id))
+                                peaks.views.getView('zoomview').enableSegmentDragging(true)
+                                moveCursorRef.current.style.cursor = 'move'
+                            }
+                        }
+                    })
+                    zoomviewContainerRef.current.addEventListener('wheel', onWheel, {passive: false})
+                    zoomviewContainerRef.current.addEventListener('keydown', (event) => {
+                        if (!props.tcLockRef.current && event.key === 'Delete') {
+                            const curSegment = props.selectedSegment.current
+                            if (curSegment) {
+                                const curRow = props.hotRef.current.getSourceDataAtCol('rowId').indexOf(curSegment.id)
+                                props.hotRef.current.setDataAtCell([[curRow, 0, ''], [curRow, 1, '']])
+                                props.selectedSegment.current = null
+                                moveCursorRef.current.style.cursor = 'default'
+                            }
+                        }
+                    })
+                    zoomviewContainerRef.current.setAttribute('tabindex', 0)
                 })
                 amplitudeScale.current = 2
                 peaks.views.getView('zoomview')?.setAmplitudeScale(amplitudeScale.current)
                 peaks.views.getView('zoomview')?.setSegmentDragMode('no-overlap')
+                peaks.views.getView('zoomview')?.enableSeek(false)
             }
         })
-        waveformRef.current.addEventListener('wheel', onWheel, {passive: false})
-        waveformRef.current.addEventListener('keydown', (event) => {
-            if (!props.tcLockRef.current && event.key === 'Delete') {
-                const curSegment = props.selectedSegment.current
-                if (curSegment) {
-                    const curRow = props.hotRef.current.getSourceDataAtCol('rowId').indexOf(curSegment.id)
-                    props.hotRef.current.setDataAtCell([[curRow, 0, ''], [curRow, 1, '']])
-                    props.selectedSegment.current = null
-                    moveCursorRef.current.style.cursor = 'default'
-                }
-            }
-        })
-        waveformRef.current.setAttribute('tabindex', 0)
         return () => {
             props.waveformRef.current?.destroy()
             props.waveformRef.current = null
@@ -214,16 +224,16 @@ const TimelineWindow = ({resetSegments, ...props}) => {
         <div className={'position-absolute end-0'} style={{zIndex: 1}}>
             <div className={'d-flex ms-auto'}>
                 <MDBCheckbox id='tcLock-checkbox' wrapperClass={'d-flex mx-2'}
-                             label='TC Lock' labelStyle={checkboxLabelStyle} checked={props.tcLock}
+                             label='TC Lock' labelClass={'timelineWindow-checkbox-label'} checked={props.tcLock}
                              onChange={(event) => {
                                  props.setTcLock(event.target.checked)
                                  event.target.blur()
                              }}/>
                 <MDBCheckbox id='playheadCenter-checkbox' wrapperClass={'d-flex mx-2'}
-                             label='Playhead Center' labelStyle={checkboxLabelStyle}
+                             label='Playhead Center' labelClass={'timelineWindow-checkbox-label'}
                              onChange={(event) => event.target.blur()}/>
                 <MDBCheckbox id='scrollView-checkbox' wrapperClass={'d-flex mx-2'}
-                             label='Current Subtitle Center' labelStyle={checkboxLabelStyle}
+                             label='Current Subtitle Center' labelClass={'timelineWindow-checkbox-label'}
                              onChange={(event) => event.target.blur()}/>
             </div>
         </div>
@@ -235,8 +245,8 @@ const TimelineWindow = ({resetSegments, ...props}) => {
                     <MDBIcon fas icon="exclamation-circle" size={'3x'} color={'white'}/>
                 </MDBBtn>
             </div>
-            <div ref={waveformRef} className={'w-100'} style={{height: `${props.size.height - 100}px`}}/>
-            <div ref={overviewRef} className={'w-100'} style={{height: '30px'}}/>
+            <div ref={zoomviewContainerRef} className={'w-100'} style={{height: `${props.size.height - 100}px`}}/>
+            <div ref={overviewContainerRef} className={'w-100'} style={{height: '30px'}}/>
         </div>
     </>
 };
